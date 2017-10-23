1a1252b65a6d05f0dd32f226ecf22850a41c8b15
Stefan Majewsky <stefan.majewsky@sap.com>
2017-10-23 12:43:56 +0200

    gofmt

--- src/syslog-stdout.go
+++ src/syslog-stdout.go
@@ -1,115 +1,149 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "log"
-    "net"
-    "os"
-    "strconv"
-    "strings"
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strconv"
+	"strings"
 )
 
 const (
-    bufferSize = 65536
-    socketPath = "/dev/log"
+	bufferSize = 65536
+	socketPath = "/dev/log"
 )
 
-type Syslog struct {}
+type Syslog struct{}
 
 func (syslog Syslog) getFacility(code int) string {
-    switch code >> 3 {
-        case 0: return "kern"
-        case 1: return "user"
-        case 2: return "mail"
-        case 3: return "daemon"
-        case 4: return "auth"
-        case 5: return "syslog"
-        case 6: return "lpr"
-        case 7: return "news"
-        case 8: return "uucp"
-        case 9: return "cron"
-        case 10: return "authpriv"
-        case 11: return "ftp"
-        case 12: return "ntp"
-        case 13: return "security"
-        case 14: return "console"
-        case 15: return "mark"
-        case 16: return "local0"
-        case 17: return "local1"
-        case 18: return "local2"
-        case 19: return "local3"
-        case 20: return "local4"
-        case 21: return "local5"
-        case 22: return "local6"
-        case 23: return "local7"
-        default: return "unknown"
-    }
+	switch code >> 3 {
+	case 0:
+		return "kern"
+	case 1:
+		return "user"
+	case 2:
+		return "mail"
+	case 3:
+		return "daemon"
+	case 4:
+		return "auth"
+	case 5:
+		return "syslog"
+	case 6:
+		return "lpr"
+	case 7:
+		return "news"
+	case 8:
+		return "uucp"
+	case 9:
+		return "cron"
+	case 10:
+		return "authpriv"
+	case 11:
+		return "ftp"
+	case 12:
+		return "ntp"
+	case 13:
+		return "security"
+	case 14:
+		return "console"
+	case 15:
+		return "mark"
+	case 16:
+		return "local0"
+	case 17:
+		return "local1"
+	case 18:
+		return "local2"
+	case 19:
+		return "local3"
+	case 20:
+		return "local4"
+	case 21:
+		return "local5"
+	case 22:
+		return "local6"
+	case 23:
+		return "local7"
+	default:
+		return "unknown"
+	}
 }
 
 func (syslog Syslog) getSeverity(code int) string {
-    switch code & 0x07 {
-        case 0: return "emerg"
-        case 1: return "alert"
-        case 2: return "crit"
-        case 3: return "err"
-        case 4: return "warning"
-        case 5: return "notice"
-        case 6: return "info"
-        case 7: return "debug"
-        default: return "unknown"
-    }
+	switch code & 0x07 {
+	case 0:
+		return "emerg"
+	case 1:
+		return "alert"
+	case 2:
+		return "crit"
+	case 3:
+		return "err"
+	case 4:
+		return "warning"
+	case 5:
+		return "notice"
+	case 6:
+		return "info"
+	case 7:
+		return "debug"
+	default:
+		return "unknown"
+	}
 }
 
 func (syslog Syslog) listen(connection net.Conn) {
-    reader := bufio.NewReader(connection)
+	reader := bufio.NewReader(connection)
 
-    for {
-        buffer := make([]byte, bufferSize)
-        size, error := reader.Read(buffer)
-        if error != nil {
-            log.Fatal("Read error:", error)
-        }
+	for {
+		buffer := make([]byte, bufferSize)
+		size, error := reader.Read(buffer)
+		if error != nil {
+			log.Fatal("Read error:", error)
+		}
 
-        go syslog.readData(buffer[0:size])
-    }
+		go syslog.readData(buffer[0:size])
+	}
 }
 
 func (syslog Syslog) readData(data []byte) {
-    header := "unknown:unknown"
-    message := string(data)
+	header := "unknown:unknown"
+	message := string(data)
 
-    endOfCode := strings.Index(message, ">")
-    if -1 != endOfCode && 5 > endOfCode {
-        code, error := strconv.Atoi(string(data[1:endOfCode]))
-        if nil == error {
-            header = fmt.Sprintf("%s:%s", syslog.getFacility(code), syslog.getSeverity(code))
-        }
+	endOfCode := strings.Index(message, ">")
+	if -1 != endOfCode && 5 > endOfCode {
+		code, error := strconv.Atoi(string(data[1:endOfCode]))
+		if nil == error {
+			header = fmt.Sprintf("%s:%s", syslog.getFacility(code), syslog.getSeverity(code))
+		}
 
-        message = string(data[endOfCode + 1:])
-    }
+		message = string(data[endOfCode+1:])
+	}
 
-    fmt.Printf("%s: %s\n", header, strings.TrimSuffix(message, "\n"))
+	fmt.Printf("%s: %s\n", header, strings.TrimSuffix(message, "\n"))
 }
 
 func (syslog Syslog) run() {
-    if _, err := os.Stat(socketPath); nil == err {
-        os.Remove(socketPath)
-    }
+	if _, err := os.Stat(socketPath); nil == err {
+		os.Remove(socketPath)
+	}
 
-    connection, error := net.ListenUnixgram("unixgram", &net.UnixAddr{socketPath, "unixgram"})
-    if nil != error {
-        log.Fatal("Listen error:", error)
-    }
+	connection, error := net.ListenUnixgram("unixgram", &net.UnixAddr{socketPath, "unixgram"})
+	if nil != error {
+		log.Fatal("Listen error:", error)
+	}
 
-    if err := os.Chmod(socketPath, 0777); nil != err {
-        log.Fatal("Impossible to change the socket permission.")
-    }
+	if err := os.Chmod(socketPath, 0777); nil != err {
+		log.Fatal("Impossible to change the socket permission.")
+	}
 
-    syslog.listen(connection)
+	syslog.listen(connection)
 }
 
 func main() {
-    syslog := Syslog{}
-    syslog.run()
+	syslog := Syslog{}
+	syslog.run()
 }
