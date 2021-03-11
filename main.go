c5cf1d49bb116553ff597d4f9333f3c600214a43
Stefan Majewsky <stefan.majewsky@sap.com>
2021-03-11 16:25:21 +0100

    style: more compact (and possible faster) facility/severity lookup

--- main.go
+++ main.go
@@ -16,87 +16,27 @@ const (
 	socketPath = "/dev/log"
 )
 
+var (
+	facilities = []string{
+		"kern", "user", "mail", "daemon", "auth", "syslog", "lpr", "news", //0..7
+		"uucp", "cron", "authpriv", "ftp", "ntp", "security", "console", "mark", //8..15
+		"local0", "local1", "local2", "local3", "local4", "local5", "local6", "local7", //16..23
+	}
+	severities = []string{
+		"emerg", "alert", "crit", "err", "warning", "notice", "info", "debug", //0..7
+	}
+)
+
+func indexInto(list []string, idx int) string {
+	if idx < 0 || idx >= len(list) {
+		return "unknown"
+	}
+	return list[idx]
+}
+
 //Syslog contains the application state.
 type Syslog struct {
 	Hostname string
-}
-
-func (syslog Syslog) getFacility(code int) string {
-	switch code >> 3 {
-	case 0:
-		return "kern"
-	case 1:
-		return "user"
-	case 2:
-		return "mail"
-	case 3:
-		return "daemon"
-	case 4:
-		return "auth"
-	case 5:
-		return "syslog"
-	case 6:
-		return "lpr"
-	case 7:
-		return "news"
-	case 8:
-		return "uucp"
-	case 9:
-		return "cron"
-	case 10:
-		return "authpriv"
-	case 11:
-		return "ftp"
-	case 12:
-		return "ntp"
-	case 13:
-		return "security"
-	case 14:
-		return "console"
-	case 15:
-		return "mark"
-	case 16:
-		return "local0"
-	case 17:
-		return "local1"
-	case 18:
-		return "local2"
-	case 19:
-		return "local3"
-	case 20:
-		return "local4"
-	case 21:
-		return "local5"
-	case 22:
-		return "local6"
-	case 23:
-		return "local7"
-	default:
-		return "unknown"
-	}
-}
-
-func (syslog Syslog) getSeverity(code int) string {
-	switch code & 0x07 {
-	case 0:
-		return "emerg"
-	case 1:
-		return "alert"
-	case 2:
-		return "crit"
-	case 3:
-		return "err"
-	case 4:
-		return "warning"
-	case 5:
-		return "notice"
-	case 6:
-		return "info"
-	case 7:
-		return "debug"
-	default:
-		return "unknown"
-	}
 }
 
 func (syslog Syslog) listen(connection net.Conn) {
@@ -130,7 +70,7 @@ func (syslog Syslog) readData(data []byte) {
 	if -1 != endOfCode && 5 > endOfCode {
 		code, err := strconv.Atoi(string(data[1:endOfCode]))
 		if err == nil {
-			facility = fmt.Sprintf("%s.%s", syslog.getFacility(code), syslog.getSeverity(code))
+			facility = fmt.Sprintf("%s.%s", indexInto(facilities, code>>3), indexInto(severities, code&0x07))
 		}
 
 		message = string(data[endOfCode+1:])
